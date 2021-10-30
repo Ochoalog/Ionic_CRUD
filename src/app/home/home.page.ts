401559a4461dd84758309298492b35d5645ad94b
Vitor.Ochoa <vitor.ochoa@sequor.com.br>
2021-10-30 15:30:28 -0300

    Crud ionic finalized

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,47 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private apiService: ApiService) {
+    //this.createData();
+    //this.readData();
+    //this.updateData();
+    this.deleteData();
+  }
 
+  readData() {
+    this.apiService.readData().subscribe(data => {
+      console.log(data);
+    });
+  }
+
+  createData() {
+    const data: any = {
+      title: 'Vitor',
+      body: 'Teste',
+      userId: 101
+    };
+
+    this.apiService.createData(data).subscribe(d => {
+      console.log(d);
+    });
+  }
+
+  updateData() {
+    const data: any = {
+      id: 1,
+      title: 'Vitor',
+      body: 'Teste',
+      userId: 10
+    };
+
+    this.apiService.updateData(data).subscribe(d => {
+      console.log(d);
+    });
+  }
+
+  deleteData() {
+    this.apiService.deleteData().subscribe(d => {
+      console.log(d);
+    });
+  }
 }
